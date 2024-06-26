9ec30c20da22652887136a8e4cc989627807bc91
lemontech119 <lemontech119@gmail.com>
2024-06-26 17:55:54 +0900

    설계중

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const indexRouter = require("./routers/index");
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use("/api", indexRouter);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
