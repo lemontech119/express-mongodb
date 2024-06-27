e8a2e3e6a441b4cb77d84109fde374b68e4acb9d
lemontech119 <lemontech119@gmail.com>
2024-06-27 17:04:26 +0900

    mongodb 세팅

--- api/index.js
+++ api/index.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
 const express = require("express");
+const mongoose = require('mongoose');
+
 const indexRouter = require("./routers/index");
 
 const app = express();
+
+const { MONGO_URI } = process.env;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 app.use("/api", indexRouter);
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
